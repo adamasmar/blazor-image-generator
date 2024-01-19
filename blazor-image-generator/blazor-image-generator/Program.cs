a26537ca78f70352b4faac4ca7df00b21350fede
Adam Asmar <adamasmar@microsoft.com>
2024-01-19 16:39:05 -0500

    Add project files.

---
+++ blazor-image-generator/blazor-image-generator/Program.cs
@@ -0,0 +1,33 @@
+using blazor_image_generator.Client.Pages;
+using blazor_image_generator.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(blazor_image_generator.Client._Imports).Assembly);
+
+app.Run();
